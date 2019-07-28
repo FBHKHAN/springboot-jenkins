441d2b8d30c152c91d8fd1f6085c44f8dae9c608
farooqkhan <fbh121@yahoo.com>
2019-07-28 19:21:41 -0400

    initial commit

---
+++ src/main/java/com/example/code4fun/Code4funApplication.java
@@ -0,0 +1,13 @@
+package com.example.code4fun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Code4funApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Code4funApplication.class, args);
+    }
+
+}
